ff30acb7d55fe35807f6e340c51cba0cf1896571
Oscar Dowson <o.dowson@gmail.com>
2026-01-15 09:58:01 +1300

    Fix copyright headers

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2018-2026: fredo-dedup, quinnj, and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 using JSON
 using JSONSchema
 using Tar
