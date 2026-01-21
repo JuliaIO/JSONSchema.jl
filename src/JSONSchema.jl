4eb82a614b7bfc89b3d0404e6f690d551128b17b
Oscar Dowson <odow@users.noreply.github.com>
2026-01-22 02:34:53 +1300

    Remove compat.jl in favor of explicitly breaking changes (#72)

--- src/JSONSchema.jl
+++ src/JSONSchema.jl
@@ -12,10 +12,7 @@ import URIs
 using JSON: JSONWriteStyle, Object
 
 export Schema, SchemaContext, ValidationResult, schema, validate
-# Backwards compatibility exports (v1.5.0)
-export diagnose, SingleIssue
 
 include("schema.jl")
-include("compat.jl")
 
 end
